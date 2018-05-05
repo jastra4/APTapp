f025441503bd739cd0ad985fc251791ece32ca1a
Joseph Strandmo <josephstrandmo@gmail.com>
2018-05-05 17:24:09 -0500

    Change intro format

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -40,19 +40,19 @@ class Search extends React.Component {
 	render() {
 		return(
 		  <div>
-			<p className="intro">
-			Type your item's number and hit enter.
-			<br></br>
-			This app pulls real world data directly from Blizzard and applies an algorithm to get you market color about your item.
-			<br></br>
-			This will help you determine a competitive price to begin auctioning your item at in world of warcraft.
-			<br></br>
-			* Due to database resource costs Blizzard data updates have been temporarily suspended.
-			</p>
+			<p className="intro"> Type your item's number and hit enter.</p>
 			
 	    	<form onSubmit={this.queryDB}>
 			    <input className="search" id="queryDB" placeholder="search by item ID (ex. 124102)"/>
 	    	</form>
+			
+			<p className="intro">
+			  This app pulls real world data directly from Blizzard and applies an algorithm to get you market color about your item.
+			  <br></br>
+			  This will help you determine a competitive price to begin auctioning your item at in world of warcraft.
+			  <br></br>
+			  * Due to database resource costs Blizzard data updates have been temporarily suspended.
+			</p>
 		  </div>
 		);
 	}

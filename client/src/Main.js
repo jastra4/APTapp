ceb72bf5cd33ece0cc6aee74918c0fd149b8eece
Joseph Strandmo <josephstrandmo@gmail.com>
2018-05-19 14:22:48 -0500

    Disable Main.js to test mobile bug

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,44 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import Store from '../src/store/store.js';
-import SearchConnected from './components/Search';
-import SummaryConnected from './components/Summary';
-import GraphConnected from './components/Graph';
-import ItemListConnected from './components/ItemList';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import Store from '../src/store/store.js';
+// import SearchConnected from './components/Search';
+// import SummaryConnected from './components/Summary';
+// import GraphConnected from './components/Graph';
+// import ItemListConnected from './components/ItemList';
 
-class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+// class Main extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
 
-  render () {
-    return (
-      <div>
-        <h2 className="header">Wowmiser
-          <div className="subHeader">A World of Warcraft app</div>
-        </h2>
-        {/* <SearchConnected /> */}
-        {/* <img src="https://i.imgur.com/L0eXr2h.jpg" id="background" alt="" /> */}
+//   render () {
+//     return (
+//       <div>
+//         <h2 className="header">Wowmiser
+//           <div className="subHeader">A World of Warcraft app</div>
+//         </h2>
+//         {/* <SearchConnected /> */}
+//         {/* <img src="https://i.imgur.com/L0eXr2h.jpg" id="background" alt="" /> */}
 
-        {/* <svg className="bar-chart" id="myGraph"></svg> */}
-        {/* <GraphConnected />   */}
+//         {/* <svg className="bar-chart" id="myGraph"></svg> */}
+//         {/* <GraphConnected />   */}
 
-        {/* <div className="test">
-          <SummaryConnected />
-        </div> */}
-        {/* <div className="test">
-          <ItemListConnected />
-        </div> */}
+//         {/* <div className="test">
+//           <SummaryConnected />
+//         </div> */}
+//         {/* <div className="test">
+//           <ItemListConnected />
+//         </div> */}
       
-      </div>
-    )
-  }
-}
+//       </div>
+//     )
+//   }
+// }
 
-ReactDOM.render((
-  <Provider store={ Store } >
-    <Main />
-  </Provider>
-), document.getElementById('root'));
+// ReactDOM.render((
+//   <Provider store={ Store } >
+//     <Main />
+//   </Provider>
+// ), document.getElementById('root'));

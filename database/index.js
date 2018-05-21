834b939b1f51d57ac5bd96a3e0c019dff1f10544
Joseph Strandmo <josephstrandmo@gmail.com>
2018-05-21 18:26:14 -0500

    Add connection to AWS RDS

--- database/index.js
+++ database/index.js
@@ -16,47 +16,47 @@ connection.connect(function (err) {
     return;
   }
 
-  console.log('Connected to mysql database.');
+  console.log('Connected to sql database.');
 });
 
-// connection.end();
+connection.end();
 
-// /************************************************************/
-// // Startup Process
-// /************************************************************/
+/************************************************************/
+// Startup Process
+/************************************************************/
 
-// const mongoose = require('mongoose');
-// mongoose.Promise = require("bluebird");
+const mongoose = require('mongoose');
+mongoose.Promise = require("bluebird");
 
-// // mongoose.connect('mongodb://localhost/edge')
-// mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true })
-//   .then(() => { console.log('✅  Successfully connected to mlab mongodb'); })
-//   .catch((e) => { console.error('⚠️ Error connected to MongoDB: ', e); });
+// mongoose.connect('mongodb://localhost/edge')
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true })
+  .then(() => { console.log('✅  Successfully connected to mlab mongodb'); })
+  .catch((e) => { console.error('⚠️ Error connected to MongoDB: ', e); });
 
-// const db = mongoose.connection;
+const db = mongoose.connection;
 
 /************************************************************/
 // Schemas
 /************************************************************/
 
-// const dumpSchema = mongoose.Schema({
-//   id: {
-//     type: String,
-//     unique: false,
-//     required: false
-//   },
-//   auc: Number,
-//   bid: Number,
-//   buyout: Number,
-//   context: Number,
-//   item: Number,
-//   owner: String,
-//   ownerRealm: String,
-//   quantity: Number,
-//   rand: Number,
-//   seed: Number,
-//   timeLeft: String,
-// });
+const dumpSchema = mongoose.Schema({
+  id: {
+    type: String,
+    unique: false,
+    required: false
+  },
+  auc: Number,
+  bid: Number,
+  buyout: Number,
+  context: Number,
+  item: Number,
+  owner: String,
+  ownerRealm: String,
+  quantity: Number,
+  rand: Number,
+  seed: Number,
+  timeLeft: String,
+});
 
 /************************************************************/
 // Insert
@@ -64,17 +64,17 @@ connection.connect(function (err) {
 const dateFormat = require('dateformat');
 
 const insertBatch = (data, stamp) => {
-  // const dumpId = JSON.stringify(dateFormat(new Date(), "isoDateTime"));
-  // mongoose.connection.db.listCollections({name: dumpId})
-  //   .next(function(err, doc) {
-  //     if (doc) {
-  //       console.log('dump already exists');
-  //     } else {
-  //       const newDump = mongoose.model(dumpId, dumpSchema);
-  //       data = JSON.parse(data);
-  //       newDump.insertMany(data.auctions);
-  //     }
-  //   });
+  const dumpId = JSON.stringify(dateFormat(new Date(), "isoDateTime"));
+  mongoose.connection.db.listCollections({name: dumpId})
+    .next(function(err, doc) {
+      if (doc) {
+        console.log('dump already exists');
+      } else {
+        const newDump = mongoose.model(dumpId, dumpSchema);
+        data = JSON.parse(data);
+        newDump.insertMany(data.auctions);
+      }
+    });
 }
 
 /************************************************************/
@@ -82,37 +82,37 @@ const insertBatch = (data, stamp) => {
 /************************************************************/
 
 var selectAll = function(item, callback) {
-  // mongoose.connection.db.listCollections().toArray(function(err, docs) {
-  //   let list = [];
-  //   let j = 0;
-  //   docs.forEach((doc, i, docs) => {
-  //     if (doc.name !== 'system.indexes') {
-  //       let col = mongoose.model(doc.name, dumpSchema);
-  //       col.find({"item": item}, (err, results) => {
-  //         if (err) {
-  //           let hist = {};
-  //           // could change null to []
-  //           hist.results = [];
-  //           hist.stamp = doc.name;
-  //           list.push(hist);
-  //           j++;
-  //           if (j === docs.length - 1) {
-  //             callback(list);
-  //           }  
-  //         } else {
-  //           let hist = {};
-  //           hist.results = results;
-  //           hist.stamp = doc.name;
-  //           list.push(hist);
-  //           j++;
-  //           if (j === docs.length-1) {
-  //             callback(list);
-  //           }  
-  //         }        
-  //       });
-  //     }
-  //   });
-  // })
+  mongoose.connection.db.listCollections().toArray(function(err, docs) {
+    let list = [];
+    let j = 0;
+    docs.forEach((doc, i, docs) => {
+      if (doc.name !== 'system.indexes') {
+        let col = mongoose.model(doc.name, dumpSchema);
+        col.find({"item": item}, (err, results) => {
+          if (err) {
+            let hist = {};
+            // could change null to []
+            hist.results = [];
+            hist.stamp = doc.name;
+            list.push(hist);
+            j++;
+            if (j === docs.length - 1) {
+              callback(list);
+            }  
+          } else {
+            let hist = {};
+            hist.results = results;
+            hist.stamp = doc.name;
+            list.push(hist);
+            j++;
+            if (j === docs.length-1) {
+              callback(list);
+            }  
+          }        
+        });
+      }
+    });
+  })
 };
 
 /************************************************************/
